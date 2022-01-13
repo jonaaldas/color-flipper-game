7536ea2116a8a90ce9875884351b0f163b9e536c
Jonathan aldas <jonathanaldas@pool-100-0-0-61.bstnma.fios.verizon.net>
2022-01-13 17:19:25 -0500

    finsihed the styles

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const arr = ['#8ecae6','#219ebc', '#023047', '#ffb703', '#fb8500'];
-console.log(arr)
+
+const btn = document.querySelector('button').addEventListener('click', function(){
+    let random = Math.floor(Math.random() * arr.length);
+    document.querySelector('span').innerText = arr[random]
+    document.querySelector('.outter').style.backgroundColor = arr[random]
+})
+
+
+
